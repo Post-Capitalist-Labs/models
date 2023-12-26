25d29e14ee4c5bd811a1e57f91cb53c44aa100db
Titan Cassini <titancassini@protonmail.com>
2023-12-26 23:17:26 +0000

    added server and viz

---
+++ council_economy/server.py
@@ -0,0 +1,41 @@
+from mesa_viz_tornado.ModularVisualization import *
+from mesa_viz_tornado.UserParam import *
+from mesa_viz_tornado.modules import CanvasGrid, ChartModule
+from mesa_viz_tornado.ModularVisualization import ModularServer
+from council_economy import CouncilBasedEconomyModel, WorkersCouncilAgent, ConsumersCouncilAgent
+
+
+def agent_portrayal(agent):
+    if agent is None:
+        return
+    portrayal = {"Shape": "circle", "Filled": "true", "r": 0.5}
+    if isinstance(agent, WorkersCouncilAgent):
+        portrayal["Color"] = "#FF0000"  # Red for WorkersCouncilAgent
+    elif isinstance(agent, ConsumersCouncilAgent):
+        portrayal["Color"] = "#0000FF"  # Blue for ConsumersCouncilAgent
+    portrayal["Layer"] = 0
+    return portrayal
+
+grid_width, grid_height = 20, 20
+canvas_element = CanvasGrid(agent_portrayal, grid_width, grid_height, 500, 500)
+
+chart_element = ChartModule([
+    {"Label": "Total Production", "Color": "Red"},
+    {"Label": "Total Consumption", "Color": "Blue"}
+])
+
+model_params = {
+    "num_workers_councils": Slider("Number of Worker Councils", 100, 1, 200, 1),
+    "num_consumers_councils": Slider("Number of Consumer Councils", 100, 1, 200, 1),
+    "worker_adjustment": Slider("Worker Adjustment", 10, 1, 20, 1),
+    "consumer_adjustment": Slider("Consumer Adjustment", 10, 1, 20, 1),
+    "width": grid_width,
+    "height": grid_height
+}
+
+server = ModularServer(
+    CouncilBasedEconomyModel,
+    [canvas_element, chart_element],
+    "Council Based Economy Model",
+    model_params
+)
