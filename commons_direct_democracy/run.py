0967988d736e6aedc9e111f414df23d72674ff0a
Titan Cassini <58642686+titancassini@users.noreply.github.com>
2024-03-05 23:40:25 +0000

    fixed UserParam implimentation

--- commons_direct_democracy/run.py
+++ commons_direct_democracy/run.py
@@ -1,6 +1,6 @@
-from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.modules import ChartModule
-from mesa_viz_tornado.UserParam import *
+from mesa_viz_tornado.ModularVisualization import ModularServer
+from mesa_viz_tornado.modules import ChartModule
+from mesa_viz_tornado.UserParam import Slider  # Assuming Slider is the correct class to use for sliders
 from direct_democracy_model import DirectDemocracyModel
 
 def agent_portrayal(agent):
@@ -13,8 +13,10 @@ chart = ChartModule([{"Label": "Resource Limit", "Color": "Black"}],
 server = ModularServer(DirectDemocracyModel,
                        [chart],
                        "Direct Democracy Model",
-                       {"N": UserParam('Slider', 'Number of Agents', 100, 10, 200, 1),
-                        "resource_limit": UserParam('Slider', 'Initial Resource Limit', 100, 50, 150, 1)})
+                       {
+                        "N": Slider('Number of Agents', 100, 10, 200, 1),
+                        "resource_limit": Slider('Initial Resource Limit', 100, 50, 150, 1)
+                       })
 
 if __name__ == '__main__':
     server.launch()
