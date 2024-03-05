98a412cfe4507a7518a24523d249868d5786ad31
Titan Cassini <58642686+titancassini@users.noreply.github.com>
2024-03-05 23:24:18 +0000

    fixed UserParam

--- commons_direct_democracy/run.py
+++ commons_direct_democracy/run.py
@@ -1,6 +1,6 @@
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
-from mesa.visualization.modules import UserSettableParameter
+from mesa.visualization.UserParam import Slider
 from direct_democracy_model import DirectDemocracyModel
 
 def agent_portrayal(agent):
@@ -13,8 +13,8 @@ chart = ChartModule([{"Label": "Resource Limit", "Color": "Black"}],
 server = ModularServer(DirectDemocracyModel,
                        [chart],
                        "Direct Democracy Model",
-                       {"N": UserSettableParameter('slider', 'Number of Agents', 100, 10, 200, 1),
-                        "resource_limit": UserSettableParameter('slider', 'Initial Resource Limit', 100, 50, 150, 1)})
+                       {"N": UserParam('slider', 'Number of Agents', 100, 10, 200, 1),
+                        "resource_limit": UserParam('slider', 'Initial Resource Limit', 100, 50, 150, 1)})
 
 if __name__ == '__main__':
     server.launch()
